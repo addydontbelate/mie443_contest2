64eb4f5a3bbcfef72c7186b45c729138d21f451c
Pranshu Malik <pranshumalik14@gmail.com>
2020-02-27 04:32:30 -0500

    first implementation of img pipeline complete!

--- src/boxes.cpp
+++ src/boxes.cpp
@@ -1,72 +1,95 @@
 #include <ros/package.h>
 #include <boxes.h>
 
-bool Boxes::load_coords() {
+bool Boxes::load_coords() 
+{
     std::string filePath = ros::package::getPath("mie443_contest2") + 
                            std::string("/boxes_database/coords.xml");
     cv::FileStorage fs(filePath, cv::FileStorage::READ);
-    if(fs.isOpened()) {
+    
+    if (fs.isOpened()) 
+    {
         cv::FileNode node;
         cv::FileNodeIterator it, end;
         std::vector<float> coordVec;
         std::string coords_xml[5] = {"coordinate1", "coordinate2", "coordinate3", "coordinate4",
                                      "coordinate5"};
-        for(int i = 0; i < 5; ++i) {
-            node = fs[coords_xml[i]];
-            if(node.type() != cv::FileNode::SEQ) {
-                std::cout << "XML ERROR: Data in " << coords_xml[i] 
+        for(const auto & i : coords_xml) 
+        {
+            node = fs[i];
+            if(node.type() != cv::FileNode::SEQ) 
+            {
+                std::cout << "XML ERROR: Data in " << i 
                           << " is improperly formatted - check input.xml" << std::endl; 
-            } else {
+            } 
+            else 
+            {
                 it = node.begin();
                 end = node.end();
                 coordVec = std::vector<float>();
-                for(int j = 0; it != end; ++it, ++j) {
+                
+                for(int j = 0; it != end; ++it, ++j)
                     coordVec.push_back((float)*it);
-                }
-                if(coordVec.size() == 3) {
+                
+                if (coordVec.size() == 3) 
+                {
                     coords.push_back(coordVec);
-                } else {
-                    std::cout << "XML ERROR: Data in " << coords_xml[i] 
+                } 
+                else 
+                {
+                    std::cout << "XML ERROR: Data in " << i 
                               << " is improperly formatted - check input.xml" << std::endl; 
                 }
             }
         }
-        if(coords.size() == 0) {
+        
+        if (coords.empty()) 
+        {
             std::cout << "XML ERROR: Coordinate data is improperly formatted - check input.xml" 
                   << std::endl;
             return false;
         }
-    } else {
+    } 
+    else 
+    {
         std::cout << "Could not open XML - check FilePath in " << filePath << std::endl;
         return false;
     }
+    
     return true;
 }
 
-bool Boxes::load_templates() {
+bool Boxes::load_templates() 
+{
     std::string filePath = ros::package::getPath("mie443_contest2") + 
                            std::string("/boxes_database/templates.xml");
     cv::FileStorage fs(filePath, cv::FileStorage::READ);
-    if(fs.isOpened()) {
-        cv::FileNode node = fs["templates"];;
-        cv::FileNodeIterator it, end;
-        if(!(node.type() == cv::FileNode::SEQ || node.type() == cv::FileNode::STRING)) { 
+    
+    if (fs.isOpened()) 
+    {
+        cv::FileNode node = fs["templates"];
+        
+        if (!(node.type() == cv::FileNode::SEQ || node.type() == cv::FileNode::STRING)) 
+        { 
             std::cout << "XML ERROR: Image data is improperly formatted in " << filePath 
                       << std::endl;
             return false;
         }
-        it = node.begin();
-        end = node.end();
-        std::string imagepath;
-        for(; it != end; ++it){
-            imagepath = ros::package::getPath("mie443_contest2") + 
+        
+        std::string img_path;
+        for(const auto & it: node)
+        {
+            img_path = ros::package::getPath("mie443_contest2") + 
                         std::string("/boxes_database/") + 
-                        std::string(*it);
-            templates.push_back(cv::imread(imagepath, CV_LOAD_IMAGE_GRAYSCALE));
+                        std::string(it);
+            templates.push_back(cv::imread(img_path, cv::IMREAD_GRAYSCALE));
         }        
-    } else {
+    } 
+    else 
+    {
         std::cout << "XML ERROR: Could not open " << filePath << std::endl;
         return false;
     }
+    
     return true;
 }
--- src/contest2.cpp
+++ src/contest2.cpp
@@ -1,34 +1,33 @@
-#include <boxes.h>
-#include <navigation.h>
-#include <robot_pose.h>
-#include <image_pipeline.h>
+#include "boxes.h"
+#include "navigation.h"
+#include "robot_pose.h"
+#include "image_pipeline.h"
 
 int main(int argc, char** argv)
 {
-    // setup ROS.
+    // setup ROS
     ros::init(argc, argv, "contest2");
     ros::NodeHandle n;
 
-    // robot pose object + subscriber.
-    RobotPose robotPose(0,0,0);
-    ros::Subscriber amclSub = n.subscribe("/amcl_pose", 1, &RobotPose::pose_callback, &robotPose);
+    // robot pose object + subscriber
+    RobotPose robot_pose(0,0,0);
+    ros::Subscriber amcl_sub = n.subscribe("/amcl_pose", 1, &RobotPose::pose_callback, &robot_pose);
 
     // initialize box coordinates and templates
     Boxes boxes; 
     if(!boxes.load_coords() || !boxes.load_templates())
     {
-        std::cout << "ERROR: could not load coords or templates" << std::endl;
-        return -1;
+        ROS_ERROR("[MAIN] Could not load coords or templates! Exiting...");
+        exit(EXIT_FAILURE);
     }
     for(int i = 0; i < boxes.coords.size(); ++i)
     {
-        std::cout << "Box coordinates: " << std::endl;
-        std::cout << i << " x: " << boxes.coords[i][0] << " y: " << boxes.coords[i][1] << " z: " 
-                  << boxes.coords[i][2] << std::endl;
+        ROS_INFO("[Main] Box %d coordinates (x: %f, y: %f, z: %f)", i,
+                boxes.coords[i][0], boxes.coords[i][1], boxes.coords[i][2]);
     }
 
     // initialize image object and subscriber
-    ImagePipeline imagePipeline(n);
+    ImagePipeline img_pipeline(n, boxes);
 
     // execute strategy
     while(ros::ok())
@@ -36,10 +35,10 @@ int main(int argc, char** argv)
         ros::spinOnce();
         /***YOUR CODE HERE***/
         // Use: boxes.coords
-        // Use: robotPose.x, robotPose.y, robotPose.phi
-        imagePipeline.get_template_ID(boxes);
+        // Use: robot_pose.x, robot_pose.y, robot_pose.phi
+        img_pipeline.get_template_ID(boxes);
         ros::Duration(0.01).sleep();
     }
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
