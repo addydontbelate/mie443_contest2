498e0514e1c2b8be57627c68cf21dc2b81a560bd
Pranshu Malik <pranshumalik14@gmail.com>
2020-02-27 04:32:30 -0500

    first implementation of img pipeline complete!

--- include/image_pipeline.h
+++ include/image_pipeline.h
@@ -5,16 +5,50 @@
 #include <opencv2/core.hpp>
 #include <cv.h>
 #include <cv_bridge/cv_bridge.h>
-#include <boxes.h>
+#include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/imgproc.hpp"
+#include "boxes.h"
+
+// # rematch tries to improve confidence in detection
+#define NUM_REMATCH 2
+#define REMATCH_THRESH 30
+#define GOOD_MATCH_DIST 0.2
+#define MIN_HESSIAN 600
+
+// possible template IDs
+enum TEMPLATE
+{
+    UNINITIALIZED = -1,
+    BLANK = 0,
+    RAISIN_BRAN = 1,
+    CIN_TOAST = 2,
+    RICE_KRISP = 3
+};
+
+struct ImageFeatures
+{
+    cv::Mat descriptors;
+    std::vector<cv::KeyPoint> keypoints;
+};
 
 class ImagePipeline
 {
  private:
-    cv::Mat img;
-    bool isValid;
+    cv::Mat scene_img;
+    bool is_valid;
     image_transport::Subscriber sub;
+    std::vector<ImageFeatures> box_features;
+    cv::Ptr<cv::xfeatures2d::SURF> detector;
+    cv::FlannBasedMatcher matcher;
+    int templateID;
+    void load_template_features(const Boxes& boxes);
+    void match_to_templates(const Boxes& boxes);
+    int match_to_template(const ImageFeatures& template_features, const ImageFeatures& feature);
+
  public:
-    explicit ImagePipeline(ros::NodeHandle& n);
+    explicit ImagePipeline(ros::NodeHandle& n, const Boxes& boxes);
     void image_callback(const sensor_msgs::ImageConstPtr& msg);
-    int get_template_ID(Boxes& boxes);
+    int get_template_ID(const Boxes& boxes);
 };
--- src/image_pipeline.cpp
+++ src/image_pipeline.cpp
@@ -1,53 +1,144 @@
 #include <image_pipeline.h>
 
 #define IMAGE_TYPE sensor_msgs::image_encodings::BGR8
-#define IMAGE_TOPIC "camera/rgb/image_raw" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
+#define IMAGE_TOPIC "camera/rgb/image_raw" // kinect:"camera/rgb/image_raw"; webcam:"camera/image"
 
-ImagePipeline::ImagePipeline(ros::NodeHandle& n)
+ImagePipeline::ImagePipeline(ros::NodeHandle& n, const Boxes& boxes)
 {
     image_transport::ImageTransport it(n);
     sub = it.subscribe(IMAGE_TOPIC, 1, &ImagePipeline::image_callback, this);
-    isValid = false;
+    is_valid = false;
+    templateID = TEMPLATE::UNINITIALIZED;
+    detector = cv::xfeatures2d::SURF::create(MIN_HESSIAN);
+
+    load_template_features(boxes);
+}
+
+void ImagePipeline::load_template_features(const Boxes& boxes)
+{
+    // store features of each box object/template to match against later
+    for (const auto& box : boxes.templates)
+    {
+        ImageFeatures features;
+        detector->detectAndCompute(box, cv::Mat(), features.keypoints, features.descriptors);
+        box_features.push_back(features);
+    }
+
+    if (box_features.empty())
+        ROS_WARN("[IMG_PIPE] No templates were contained in boxes! No features added.");
 }
 
 void ImagePipeline::image_callback(const sensor_msgs::ImageConstPtr& msg)
 {
     try
     {
-        if(isValid)
-            img.release();
+        if(is_valid)
+            scene_img.release();
 
-        img = (cv_bridge::toCvShare(msg, IMAGE_TYPE)->image).clone();
-        isValid = true;
+        scene_img = (cv_bridge::toCvShare(msg, IMAGE_TYPE)->image).clone();
+        is_valid = true;
     }
     catch (cv_bridge::Exception& e)
     {
-        std::cout << "ERROR: Could not convert from " << msg->encoding.c_str()
-                  << " to " << IMAGE_TYPE.c_str() << "!" << std::endl;
-        isValid = false;
-    }    
+        ROS_ERROR("[IMG_PIPE] Could not convert from %s to %s!",
+                msg->encoding.c_str(), IMAGE_TYPE.c_str());
+        is_valid = false;
+    }
 }
 
-int ImagePipeline::get_template_ID(Boxes& boxes) {
-    int template_id = -1;
+int ImagePipeline::get_template_ID(const Boxes& boxes)
+{
+    // reset template ID
+    templateID = TEMPLATE::UNINITIALIZED;
+    ros::spinOnce();
 
-    if (!isValid)
+    // check if scene image is valid
+    if (!is_valid)
     {
-        std::cout << "ERROR: INVALID IMAGE!" << std::endl;
+        ROS_ERROR("[IMG_PIPE] Invalid scene image! Skipping detection procedure.");
     }
-    else if(img.empty() || img.rows <= 0 || img.cols <= 0)
+    else if(scene_img.empty() || scene_img.rows <= 0 || scene_img.cols <= 0)
     {
-        std::cout << "ERROR: VALID IMAGE, BUT STILL A PROBLEM EXISTS!" << std::endl;
-        std::cout << "img.empty():" << img.empty() << std::endl;
-        std::cout << "img.rows:" << img.rows << std::endl;
-        std::cout << "img.cols:" << img.cols << std::endl;
+        ROS_ERROR("[IMG_PIPE] Valid scene image but a problem exists!");
+        ROS_INFO("scene_img.empty(): %s", scene_img.empty() ? "true" : "false");
+        ROS_INFO("scene_img.rows: %d", scene_img.rows);
+        ROS_INFO("scene_img.cols: %d", scene_img.cols);
     }
     else
     {
-        /***YOUR CODE HERE***/
-        // Use: boxes.templates
-        cv::imshow("view", img);
+        // send current scene
+        cv::imshow("view", scene_img);
         cv::waitKey(10);
+
+        // find a match and update templateID
+        match_to_templates(boxes);
     }
-    return template_id;
+
+    if (templateID != TEMPLATE::BLANK || templateID != TEMPLATE::UNINITIALIZED)
+    {
+        cv::imshow("view", boxes.templates[templateID-1]);
+        cv::waitKey(2000); // display detected template for 2 seconds
+    }
+
+    return templateID;
+}
+
+void ImagePipeline::match_to_templates(const Boxes& boxes)
+{
+    // detect scene features
+    ImageFeatures scene_features;
+    detector->detectAndCompute(scene_img, cv::Mat(), scene_features.keypoints, scene_features.descriptors);
+
+    // match against box object/template features
+    int rematches_reqd = 0, box_idx = 0;
+    std::vector<int> num_matches(box_features.size(), 0);
+
+    for (const auto& template_features : box_features)
+        num_matches[(box_idx++)] = match_to_template(template_features, scene_features);
+
+    // get maximum of the match
+    auto max_match = std::max_element(num_matches.begin(), num_matches.end());
+
+    // while best match is less than rematch thresh, run rematching
+    while (*max_match < REMATCH_THRESH && rematches_reqd < NUM_REMATCH)
+    {
+        ROS_INFO("[IMG_PIPE] Running a rematch");
+        ros::spinOnce();
+        detector->detectAndCompute(scene_img, cv::Mat(), scene_features.keypoints, scene_features.descriptors);
+
+        // rematch
+        box_idx = 0;
+        for (const auto& template_features : box_features)
+            num_matches[(box_idx++)] = match_to_template(template_features, scene_features);
+
+        max_match = std::max_element(num_matches.begin(), num_matches.end());
+        rematches_reqd++;
+    }
+
+    // if at the end of rematching, best match was less than low match thresh, then return blank
+    if (*max_match < REMATCH_THRESH)
+        templateID = TEMPLATE::BLANK;
+    else
+    {
+        // template ID corresponds to idx of maximum + 1 since BLANK is at 0 in TEMPLATE
+        int idx = std::distance(num_matches.begin(), max_match);
+        templateID = TEMPLATE(idx + 1);
+    }
+}
+
+int ImagePipeline::match_to_template(const ImageFeatures& template_features, const ImageFeatures& scene_features)
+{
+    // feature matching
+    std::vector<cv::DMatch> matches;
+    matcher.match(template_features.descriptors, scene_features.descriptors, matches);
+
+    // count "good" matches
+    int num_good_matches = 0;
+    for (const auto& match : matches)
+    {
+        if (match.distance < GOOD_MATCH_DIST)
+            num_good_matches++;
+    }
+
+    return num_good_matches;
 }
