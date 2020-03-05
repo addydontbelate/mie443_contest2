b6f76a0c960c669b1abff24982ef9f48c7756268
Pranshu <pranshumalik14@gmail.com>
2020-03-05 18:45:58 -0500

    ready to test basic contest2 functionality
    Co-authored-by: treppomj27 <mjt2727@gmail.com>

--- include/navigation.h
+++ include/navigation.h
@@ -5,18 +5,27 @@
 #include "boxes.h"
 #include "robot_pose.h"
 
-#define OFFSET 2
+#define IMG_CAPTURE_OFFSET 0.55
+#define VISITED true
+#define UNVISITED false
 
 class Navigation
 {
  private:
     RobotPose rob_pose;
-    std::vector<std::vector<float>> obj_unvisited;
-    std::vector<std::vector<float>> obj_opt_seq;   // optimal objective sequence
-    std::map<std::vector<float>, bool> obj_state;  // state of objectives (unvisited: FALSE, visited: TRUE)
+    RobotPose init_rob_pose;
+    std::vector<std::vector<float>> obj_unvisited; 
+    std::vector<std::vector<float>> obj_opt_seq;      // optimal objective sequence
+    std::vector<std::vector<float>> goal_opt_seq;     // optimal goal sequence (objectives with offset)
+    std::map<std::vector<float>, bool> obj_state;     // state of objectives (UNVISITED/VISITED)
+    std::map<std::vector<float>, int> obj_box_idx;   // corresponding box ids for objectives
 
  public:
     static bool move_to_goal(float goal_x, float goal_y, float goal_phi);
     void compute_opt_seq();
+    std::vector<std::vector<float>> get_goal_seq();
+    int get_obj_ID(std::vector<float> obj);
+    void set_obj_visited(int obj_opt_seq_idx);
+    bool any_unvisited_obj(); 
     Navigation(ros::NodeHandle& nh, const Boxes& boxes);
 };
